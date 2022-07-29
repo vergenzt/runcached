9f965e14d853df033a4a3c6ccab36abc7362a7a3
Tim Vergenz <vergenzt@gmail.com>
2022-07-29 11:13:09 -0400

    wip

--- runcached/main.py
+++ runcached/main.py
@@ -5,13 +5,14 @@ import shlex
 import sys
 from dataclasses import dataclass, field, replace
 from datetime import datetime
+from enum import Enum
 from functools import cached_property, partial
 from hashlib import sha256
-from subprocess import run
-from typing import Callable, List, Mapping, Optional, cast
+from typing import IO, Callable, List, Mapping, Optional, cast
 
 import appdirs
 import diskcache
+import sh
 
 from .args import CliArgs, EnvArg
 
@@ -20,21 +21,34 @@ from .args import CliArgs, EnvArg
 STRIP_ANSI_ESCAPES: Callable[[str], str] = partial(re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])').sub, '')
 
 
+class OutputDest(Enum):
+  OUT = 'stdout'
+  ERR = 'stderr'
+
+  @property
+  def io(self) -> IO[str]:
+    return getattr(sys, self.value)
+
+
+@dataclass(frozen=True)
+class OutputLine:
+  dest: OutputDest
+  text: str
+
+  def write(self, filter: Callable[[str], str] = str) -> 'OutputLine':
+    self.dest.io.write(filter(self.text))
+    return self
+
+
 @dataclass(frozen=True)
 class RunResult:
   started_at: datetime
   return_code: int
-  stdout: Optional[str] = None
-  stderr: Optional[str] = None
+  output_lines: List[OutputLine]
 
-  def write(self) -> int:
-    for f, val in [(sys.stdout, self.stdout), (sys.stderr, self.stderr)]:
-      if val:
-        try:
-          f.write(val)
-        except BrokenPipeError:
-          pass
-    return self.return_code
+  def replay(self, filter: Callable[[str], str] = str):
+    for line in self.output_lines:
+      line.write(filter)
 
 
 @dataclass(frozen=True)
@@ -48,23 +62,37 @@ class RunConfig:
   strip_colors: bool = False
   custom_cache_key: Optional[str] = None
 
+  @property
+  def _output_filter(self) -> Callable[[str], str]:
+    return STRIP_ANSI_ESCAPES if self.strip_colors else str
+
+  def _args(self) -> List[str]:
+    if self.shell:
+      joiner: Callable[[List[str]], str] = shlex.join if self.shlex else ' '.join
+      return [os.environ['SHELL'], '-c', joiner(self.command)]
+    else:
+      return self.command
+
   def _run_without_caching(self) -> 'RunResult':
     started_at = datetime.now()
-    result = run(
-      args=(shlex.join if self.shlex else ' '.join)(self.command) if self.shell else self.command,
-      shell=self.shell,
-      executable=os.environ.get('SHELL') if self.shell else None,
-      env={ **self.envs_for_cache, **self.envs_for_passthru},
-      input=self.input,
-      text=True,
-      capture_output=True,
+    args = self._args()
+    output_lines: List[OutputLine] = list()
+    command = sh.Command(args[0]).bake(*args[1:])
+    process: sh.RunningCommand = command(
+      _env={ **self.envs_for_cache, **self.envs_for_passthru },
+      _in=self.input,
+      _out=lambda line: output_lines.append(OutputLine(OutputDest.OUT, line).write(self._output_filter)),
+      _err=lambda line: output_lines.append(OutputLine(OutputDest.ERR, line).write(self._output_filter)),
+      _truncate_exc=False,
     )
-    return RunResult(
-      started_at,
-      result.returncode,
-      STRIP_ANSI_ESCAPES(result.stdout) if self.strip_colors else result.stdout,
-      result.stderr
-    )
+
+    try:
+      process.wait()
+    except sh.ErrorReturnCode:
+      pass
+
+    assert isinstance(process.exit_code, int)
+    return RunResult(started_at, process.exit_code, output_lines)
 
   @cached_property
   def _cacheable(self) -> 'RunConfig':
@@ -83,6 +111,8 @@ class RunConfig:
 
     if (result := cast(RunResult, cache.get(self._cacheable))) and result.started_at >= min_started_at:
       logging.info(f'Using cached result for {self} from {result.started_at}.')
+      result.replay(self._output_filter)
+
     elif result := self._run_without_caching():
       if result.return_code == 0 or args.keep_failures:
         cache.set(self._cacheable, result)
@@ -91,11 +121,26 @@ class RunConfig:
 
     return result
 
+
+def _sh_command_info_to_debug(log: logging.LogRecord):
+  'Redirect all INFO messages to DEBUG'
+  if log.levelno == logging.INFO:
+    log.levelno, log.levelname = logging.DEBUG, 'DEBUG'
+    logger = logging.getLogger('sh.command')
+    if logger.isEnabledFor(logging.DEBUG):
+      logger.handle(log)
+    return False
+  else:
+    return True
+
+
 def cli(argv = sys.argv[1:]) -> int:
   logging.basicConfig(format='[runcached:%(levelname)s] %(message)s')
+  logging.getLogger('sh.command').addFilter(_sh_command_info_to_debug)
+
+  # short circuit so we can debug the argument/environment parsing itself
   if {'-v', '--verbose'} & set(argv) or os.environ.get('RUNCACHED_VERBOSE') or os.environ.get('RUNCACHED_v'):
     logging.getLogger().setLevel(logging.DEBUG)
-    sys.addaudithook(lambda *a: print('[runcached:DEBUG]', *a, file=sys.stderr) if a[0] == 'subprocess.Popen' else None)
 
   args, parser = CliArgs.parse(argv)
 
@@ -122,8 +167,7 @@ def cli(argv = sys.argv[1:]) -> int:
   cache = diskcache.Cache(cache_dir)
 
   if cfg.command:
-    result = cfg.run_with_caching(cache, args)
-    return result.write()
+    return cfg.run_with_caching(cache, args).return_code
   else:
     parser.print_help()
     return 1
--- runcached/test_main.py
+++ runcached/test_main.py
@@ -1,28 +1,50 @@
 import os
 import re
+from json import JSONEncoder, dumps
 from pathlib import Path
 from subprocess import PIPE, run
-from typing import Dict, Iterator
+from typing import Any, Callable, Dict, Iterator, Tuple
+from freezegun import freeze_time
 
+import sh
 from pytest import fixture
+from syrupy.assertion import SnapshotAssertion
 
 
 @fixture
 def testenv(tmp_path: Path) -> Iterator[Dict[str, str]]:
   yield {
     'HOME': str(tmp_path),
-    'PATH': os.environ.get('PATH'),
+    'PATH': os.environ['PATH'],
   }
 
+@fixture
+def cli(testenv: Dict[str, str]) -> Iterator[sh.Command]:
+  yield sh.Command('runcached').bake(
+    '--quiet',
+    _in='',
+    _env=testenv,
+    _truncate_exc=False,
+  )
 
-def test_main(testenv: Dict[str, str]):
-  result = run(['runcached', '--', 'echo', 'foo'], text=True, input='', stdout=PIPE, stderr=PIPE, env=testenv)
-  assert result.returncode == 0
-  assert result.stdout == 'foo\n'
-  assert result.stderr == ''
+res: Callable[[sh.RunningCommand], Tuple] = lambda of: (of.exit_code, of.stdout, of.stderr)
 
-  result = run(['runcached', '--', 'echo', 'foo'], text=True, input='', stdout=PIPE, stderr=PIPE, env=testenv)
-  assert result.returncode == 0
-  assert result.stdout == 'foo\n'
-  assert re.match(string=result.stderr, pattern='^' + re.escape("[runcached:INFO] Using cached result") + '.*$')
+@fixture
+def rc_random(cli: sh.Command):
+  return cli.bake('--ttl=60s', '--', 'head', '-c5', '/dev/random')
 
+
+def test_random_within_ttl(rc_random: sh.Command):
+  assert res(rc_random()) == res(rc_random())
+
+def test_random_beyond_ttl(rc_random: sh.Command):
+  with freeze_time() as freezer:
+    assert (init_res := res(rc_random())) == res(rc_random())
+    freezer.tick(61)
+    assert res(rc_random()) != init_res
+
+
+def test_interleave(cli: sh.Command, snapshot: SnapshotAssertion):
+  cmd = cli.bake('--', 'sh', '-c', 'echo foo; echo bar >&2; echo baz; echo boz >&2')
+  assert res(cmd()) == res(cmd()) == snapshot()
+    
