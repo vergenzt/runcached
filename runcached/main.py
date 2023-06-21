192f37159a86c65d1da672fd6d020a8de43132fd
Tim Vergenz <vergenzt@gmail.com>
2023-06-21 12:10:37 -0400

    Show incremental output on cache miss & keep output order

--- runcached/main.py
+++ runcached/main.py
@@ -1,3 +1,7 @@
+from asyncio import StreamReader, Task, create_task, run as async_run, wait
+from asyncio.subprocess import PIPE, DEVNULL, Process, create_subprocess_shell, create_subprocess_exec
+from enum import Enum
+from io import StringIO
 import logging
 import os
 import re
@@ -7,8 +11,7 @@ from dataclasses import dataclass, field, replace
 from datetime import datetime
 from functools import cached_property, partial
 from hashlib import sha256
-from subprocess import run
-from typing import Callable, List, Mapping, Optional, cast
+from typing import IO, AsyncIterator, Callable, Dict, List, Mapping, Optional, TypeAlias, cast
 
 import appdirs
 import diskcache
@@ -16,25 +19,68 @@ import diskcache
 from .args import CliArgs, EnvArg
 
 
+StrFilter: TypeAlias = Callable[[str], str]
+
 # https://stackoverflow.com/a/14693789
-STRIP_ANSI_ESCAPES: Callable[[str], str] = partial(re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])').sub, '')
+STRIP_ANSI_ESCAPES: StrFilter = partial(re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])').sub, '')
+
+
+class OutputDest(Enum):
+  OUT = 'stdout'
+  ERR = 'stderr'
+
+  @property
+  def io(self) -> IO[str]:
+    return getattr(sys, self.value)
+
+  def reader_for(self, proc: Process) -> StreamReader:
+    return getattr(proc, self.value)
+
+
+@dataclass(frozen=True)
+class Output:
+  dest: OutputDest
+  text: str
+
+  @classmethod
+  async def from_process(cls, proc: Process) -> AsyncIterator['Output']:
+    iters: Dict[str, AsyncIterator[bytes]] = { dest.value: aiter(dest.reader_for(proc)) for dest in OutputDest }
+    nexts: Dict[str, Task[bytes]] = {}
+
+    while iters:
+      nexts.update({
+        dest: create_task(anext(iter), name=dest)
+        for dest, iter in iters.items() if dest not in nexts
+      })
+
+      nexts_done, _nexts_pending = await wait(nexts.values(), return_when='FIRST_COMPLETED')
+
+      for next_done in nexts_done:
+        next_dest = next_done.get_name()
+        nexts.pop(next_dest)
+
+        try:
+          next_line = await next_done
+          yield Output(OutputDest(next_dest), next_line.decode())
+        except StopAsyncIteration:
+          iters.pop(next_dest)
+
+  def write(self, filter: StrFilter = str):
+    self.dest.io.write(filter(self.text))
 
 
 @dataclass(frozen=True)
 class RunResult:
   started_at: datetime
   return_code: int
-  stdout: Optional[str] = None
-  stderr: Optional[str] = None
+  outputs: List[Output]
 
-  def write(self) -> int:
-    for f, val in [(sys.stdout, self.stdout), (sys.stderr, self.stderr)]:
-      if val:
-        try:
-          f.write(val)
-        except BrokenPipeError:
-          pass
-    return self.return_code
+  def replay_outputs(self, filter: StrFilter = str):
+    for output in self.outputs:
+      try:
+        output.write(filter)
+      except BrokenPipeError:
+        pass
 
 
 @dataclass(frozen=True)
@@ -48,22 +94,43 @@ class RunConfig:
   strip_colors: bool = False
   custom_cache_key: Optional[str] = None
 
-  def _run_without_caching(self) -> 'RunResult':
+  @cached_property
+  def _output_filter(self):
+    return STRIP_ANSI_ESCAPES if self.strip_colors else (lambda s: s)
+
+  async def _run_without_caching(self) -> 'RunResult':
     started_at = datetime.now()
-    result = run(
-      args=(shlex.join if self.shlex else ' '.join)(self.command) if self.shell else self.command,
-      shell=self.shell,
+
+    if self.shell:
+      run = create_subprocess_shell
+      if self.shlex:
+        args = [shlex.join(self.command)]
+      else:
+        args = [' '.join(self.command)]
+    else:
+      run = create_subprocess_exec
+      args = self.command
+
+    proc = await run(
+      *args,
       executable=os.environ.get('SHELL') if self.shell else None,
       env={ **self.envs_for_cache, **self.envs_for_passthru },
-      input=self.input,
-      text=True,
-      capture_output=True,
+      stdin = DEVNULL if self.input is None else StringIO(self.input),
+      stdout = PIPE,
+      stderr = PIPE,
     )
+
+    outputs = []
+    async for output in Output.from_process(proc):
+      outputs.append(output)
+      output.write(self._output_filter)
+
+    return_code = await proc.wait()
+
     return RunResult(
       started_at,
-      result.returncode,
-      STRIP_ANSI_ESCAPES(result.stdout) if self.strip_colors else result.stdout,
-      result.stderr
+      return_code,
+      outputs,
     )
 
   @cached_property
@@ -77,21 +144,24 @@ class RunConfig:
       },
     )
 
-  def run_with_caching(self, cache: diskcache.Cache, args: CliArgs) -> 'RunResult':
+  async def run_with_caching(self, cache: diskcache.Cache, args: CliArgs) -> 'RunResult':
     logging.debug(self)
     min_started_at = datetime.now() - args.ttl
 
     if (result := cast(RunResult, cache.get(self._cacheable))) and result.started_at >= min_started_at:
       logging.info(f'Using cached result for {self} from {result.started_at}.')
-    elif result := self._run_without_caching():
+      result.replay_outputs(self._output_filter)
+      return result
+    else:
+      result = await self._run_without_caching()
       if result.return_code == 0 or args.keep_failures:
         cache.set(self._cacheable, result)
       else:
         logging.warn(f'Command returned {result.return_code} and --keep-failures not specified; refusing to cache.')
+      return result
 
-    return result
 
-def cli(argv: List[str] = sys.argv[1:]) -> int:
+async def cli(argv: List[str] = sys.argv[1:]) -> int:
   logging.basicConfig(format='[runcached:%(levelname)s] %(message)s')
   if {'-v', '--verbose'} & set(_args_before_doubledash := argv[:(argv.index('--') if '--' in argv else -1)]) \
       or os.environ.get('RUNCACHED_VERBOSE') \
@@ -124,14 +194,14 @@ def cli(argv: List[str] = sys.argv[1:]) -> int:
   cache = diskcache.Cache(cache_dir)
 
   if cfg.command:
-    result = cfg.run_with_caching(cache, args)
-    return result.write()
+    result = await cfg.run_with_caching(cache, args)
+    return result.return_code
   else:
     parser.print_help()
     return 1
 
 def main():
-  sys.exit(cli())
+  sys.exit(async_run(cli()))
 
 if __name__=='__main__':
   main()
